82979597cab89693a85be8fe82eb3f1674341b8f
Humberto Diaz <heleazardm@gmail.com>
2025-03-14 13:38:10 -0500

    variables added

--- VScode_script.py
+++ VScode_script.py
@@ -1,2 +1,15 @@
 print("Hello, World!")
 
+# Defining birth date variable
+
+year = 1998
+month = 10
+day = 14
+
+# Calculating sum and mean 
+total = year + month + day
+mean = total / 3
+
+# Printing the outcome
+print(f"summatory of dates: {total}")
+print(f"mean of summ dates: {mean}")
